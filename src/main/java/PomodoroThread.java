e1d9c6e29558930a3afbffb65a9cb5e3d89365e3
Nguyen Le <nvl.nguyenle@gmail.com>
2018-02-23 14:13:00 -0800

    fixed timer

--- src/main/java/PomodoroThread.java
+++ src/main/java/PomodoroThread.java
@@ -1,48 +1,70 @@
-import javax.swing.*;
-import java.awt.*;
-
 /**
  * @author Nguyen Le
- * @since February 21, 2:15 PM
+ * @since February 21, 1:52 PM
  */
-public class PomodoroThread implements Runnable {
 
-    private int time = 26;
-    private int longBreak = 0;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.lang.Thread;
+public class Solution extends Thread {
 
-    public void run() {
+    static boolean running = false;
+    static boolean paused = true;
+    static boolean begin = false;
+
+    public static void main(String[] args) {
+        running = true;
         JFrame frame = new JFrame("Pomodoro Timer");
+
+        JButton button = new JButton("Begin Timer!");
+
+        JPanel panel = new JPanel();
+        JTextArea testArea = new JTextArea("Pomodoro Timer");
+        JTextArea testArea2 = new JTextArea("25 Mins & 00 Seconds");
+
+        // Add button to JPanel
+        panel.add(testArea);
+        panel.add(testArea2);
+        panel.add(button);
+        // And JPanel needs to be added to the JFrame itself!
+        frame.getContentPane().add(panel);
+        frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(Toolkit.getDefaultToolkit().getScreenSize().width/4,Toolkit.getDefaultToolkit().getScreenSize().height/2);
-        frame.setVisible(true);
-        while (true) {
-            if (!Solution.running) {
-                return;
-            }
 
-            if (!Solution.paused) {
-                // Every time this runs...
-                --time;
-                System.out.println("Mins: " + this.time/60 + " Seconds: " + this.time%60);
-            }
+        PomodoroThread p = new PomodoroThread();
+//        Thread t1= new Thread(new PomodoroThread());
+        Thread t1= new Thread(p);
 
-            if(this.time == 0 && longBreak  != 4){
-                System.out.println("break time for 3 mins");
-                this.time = 180;
-                longBreak++;
 
-            }
-            else if(this.time == 0){
-                longBreak =0;
-                System.out.println("break time for 15 mins");
-                this.time = 900;
-            }
 
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
+
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(!begin){
+                    begin = true;
+                }
+                if(paused){
+                    System.out.println("Timer resumed with " + p.time/60 + " mins " +  p.time%60 + " seconds left");
+                    button.setText("Pause");
+                    paused  = false;
+                }
+                else{
+                    System.out.println("Now paused with "  + p.time/60 + " mins " +  p.time%60 + " seconds remaining");
+                    button.setText("Resume");
+                    paused = true;
+                }
             }
-        }
+        });
+
+        t1.run();
+
+
+//        abc.interrupt();
     }
+
 
 }

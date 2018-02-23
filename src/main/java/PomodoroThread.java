7246d8430910ee74f388071467f8f8aa2ee9ae19
Nguyen Le <nvl.nguyenle@gmail.com>
2018-02-23 14:28:04 -0800

    fixing overwritten files

--- src/main/java/PomodoroThread.java
+++ src/main/java/PomodoroThread.java
@@ -7,42 +7,57 @@ import java.awt.*;
  */
 public class PomodoroThread implements Runnable {
 
-    private int time = 26;
+    protected int time = 1500;
     private int longBreak = 0;
+    private boolean currentBreak = false;
 
     public void run() {
-        JFrame frame = new JFrame("Pomodoro Timer");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(Toolkit.getDefaultToolkit().getScreenSize().width/4,Toolkit.getDefaultToolkit().getScreenSize().height/2);
-        frame.setVisible(true);
+//        JFrame frame = new JFrame("Pomodoro Timer");
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.setSize(Toolkit.getDefaultToolkit().getScreenSize().width/4,Toolkit.getDefaultToolkit().getScreenSize().height/2);
+//        frame.setVisible(true);
         while (true) {
-            if (!Solution.running) {
-                return;
-            }
+            while(!Solution.paused && Solution.begin == true){
+                if (!Solution.running) {
+                    return;
+                }
 
-            if (!Solution.paused) {
-                // Every time this runs...
-                --time;
-                System.out.println("Mins: " + this.time/60 + " Seconds: " + this.time%60);
-            }
+                if (!Solution.paused) {
+                    // Every time this runs...
+                    --time;
+                    if(currentBreak){
+                        System.out.println("CURRENTLY ON BREAK: Mins: " + this.time/60 + " Seconds: " + this.time%60 + " Remaining");
+                    }
+                    else{
+                        System.out.println("Mins: " + this.time/60 + " Seconds: " + this.time%60 + " Remaining");
+                    }
+                }
+                else{
+                    System.out.println("Currently paused at with ");
+                }
 
-            if(this.time == 0 && longBreak  != 4){
-                System.out.println("break time for 3 mins");
-                this.time = 180;
-                longBreak++;
+                if(this.time == 0 && longBreak  != 4){
+                    System.out.println("break time for 3 mins");
+                    this.time = 180;
+                    longBreak++;
 
-            }
-            else if(this.time == 0){
-                longBreak =0;
-                System.out.println("break time for 15 mins");
-                this.time = 900;
-            }
-
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
+                }
+                else if(this.time == 0){
+                    longBreak =0;
+                    System.out.println("break time for 15 mins");
+                    this.time = 900;
+                }
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    System.out.println("GOT IN HERE");
+                    Solution.paused = true;
+                }
             }
         }
+    }
+    public void suspend(){
+
     }
 
 }

6c03e604aa4019d8d2b6f175c7d747afeb0ceaf5
Nguyen Le <nvl.nguyenle@gmail.com>
2018-02-26 00:43:00 -0800

    Timer logic is working correctly, able to pause/resume timer of breaks and work time

--- src/main/java/PomodoroThread.java
+++ src/main/java/PomodoroThread.java
@@ -7,50 +7,68 @@ import java.awt.*;
  */
 public class PomodoroThread implements Runnable {
 
-    protected int time = 1500;
+//    protected int time = 1500;
+    protected int time = 5;
     private int longBreak = 0;
     private boolean currentBreak = false;
-    Frame frame;
 
     public void run() {
         while (true) {
             while(!Solution.paused && Solution.begin == true){
-                if (!Solution.running) {
-                    return;
-                }
 
-                if (!Solution.paused) {
-                    // Every time this runs...
                     --time;
                     if(currentBreak){
-                        System.out.println("CURRENTLY ON BREAK: Mins: " + this.time/60 + " Seconds: " + this.time%60 + " Remaining");
-
+                        Solution.testArea2.setText("BREAK " + Integer.toString(longBreak) +": "
+                                + Integer.toString( this.time/60) +
+                                " Mins & "  + Integer.toString(this.time%60) + " Seconds " );
                     }
                     else{
-                        Solution.testArea2.setText( Integer.toString( this.time/60) +  " Mins & "  + Integer.toString(this.time%60) + " Seconds "  );
+                        Solution.testArea2.setText( Integer.toString( this.time/60)
+                                +  " Mins & "  + Integer.toString(this.time%60) + " Seconds "  );
                     }
-                }
-                else{
-                    System.out.println("Currently paused at with ");
-                }
 
-                if(this.time == 0 && longBreak  != 4){
-                    System.out.println("break time for 3 mins");
-                    this.time = 180;
+                //if time is 0 and is NOT the 5th break and NOT already on break
+                if(this.time == 0 && longBreak  != 4 && !currentBreak){
+//                    this.time = 180;
+                    this.time = 10;
                     longBreak++;
-
+                    currentBreak = true;
+                    Solution.paused = true;
+                    Solution.button.setText("Begin Break");
+                    Solution.testArea2.setText("3 Mins & 0 Seconds");
                 }
-                else if(this.time == 0){
+                // if the time is 0 and IS the 5th break while NOT being on break
+                else if(this.time == 0 && !currentBreak){
                     longBreak =0;
-                    System.out.println("break time for 15 mins");
-                    this.time = 900;
+//                    this.time = 900;
+                    this.time = 15;
+                    currentBreak = true;
+                    Solution.paused = true;
+                    Solution.button.setText("Begin Break");
+                    Solution.testArea2.setText("15 Mins & 0 Seconds");
                 }
+                //if the time is 0 and NOT on break
+                else if(this.time == 0){
+                    currentBreak = false;
+                    Solution.paused = true;
+//                    this.time = 1500;
+                    this.time = 25;
+                    Solution.button.setText("Resume Work");
+                    Solution.testArea2.setText(Integer.toString( this.time/60)
+                            +  " Mins & "  + Integer.toString(this.time%60) + " Seconds ");
+                }
+
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     System.out.println("GOT IN HERE");
                     Solution.paused = true;
                 }
+            }
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
             }
         }
     }
--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -13,17 +13,17 @@ public class Solution extends Thread {
     static boolean running = false;
     static boolean paused = true;
     static boolean begin = false;
-    static JTextArea testArea2 =new JTextArea("25 Mins & 00 Seconds");;
+    static JTextArea testArea2 =new JTextArea("25 Mins & 00 Seconds");
+    static JButton button = new JButton("Begin Timer!");
 
     public static void main(String[] args) {
         running = true;
         JFrame frame = new JFrame("Pomodoro Timer");
 
-        JButton button = new JButton("Begin Timer!");
+
 
         JPanel panel = new JPanel();
         JTextArea testArea = new JTextArea("Pomodoro Timer");
-//        JTextArea testArea2 = new JTextArea("25 Mins & 00 Seconds");
 
         // Add button to JPanel
         panel.add(testArea);
@@ -36,7 +36,6 @@ public class Solution extends Thread {
         frame.setSize(Toolkit.getDefaultToolkit().getScreenSize().width/4,Toolkit.getDefaultToolkit().getScreenSize().height/2);
 
         PomodoroThread p = new PomodoroThread();
-//        Thread t1= new Thread(new PomodoroThread());
         Thread t1= new Thread(p);
 
 
@@ -49,12 +48,10 @@ public class Solution extends Thread {
                     begin = true;
                 }
                 if(paused){
-                    System.out.println("Timer resumed with " + p.time/60 + " mins " +  p.time%60 + " seconds left");
                     button.setText("Pause");
                     paused  = false;
                 }
                 else{
-                    System.out.println("Now paused with "  + p.time/60 + " mins " +  p.time%60 + " seconds remaining");
                     button.setText("Resume");
                     paused = true;
                 }
@@ -62,9 +59,6 @@ public class Solution extends Thread {
         });
 
         t1.run();
-
-
-//        abc.interrupt();
     }
 
 

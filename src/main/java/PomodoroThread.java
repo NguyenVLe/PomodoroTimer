0416a69f34f8520b43c6c875bc61482693584043
Nguyen Le <nvl.nguyenle@gmail.com>
2018-02-26 10:55:44 -0800

    added sound when timer is finished for both work and break.

--- src/main/java/PomodoroThread.java
+++ src/main/java/PomodoroThread.java
@@ -11,59 +11,64 @@ public class PomodoroThread implements Runnable {
     protected int time = 5;
     private int longBreak = 0;
     private boolean currentBreak = false;
+    private boolean timeFinish = false;
 
     public void run() {
         while (true) {
             while(!Solution.paused && Solution.begin == true){
-
-                    --time;
-                    if(currentBreak){
-                        Solution.testArea2.setText("BREAK " + Integer.toString(longBreak) +": "
-                                + Integer.toString( this.time/60) +
-                                " Mins & "  + Integer.toString(this.time%60) + " Seconds " );
-                    }
-                    else{
-                        Solution.testArea2.setText( Integer.toString( this.time/60)
-                                +  " Mins & "  + Integer.toString(this.time%60) + " Seconds "  );
-                    }
-
-                //if time is 0 and is NOT the 5th break and NOT already on break
-                if(this.time == 0 && longBreak  != 4 && !currentBreak){
-//                    this.time = 180;
-                    this.time = 10;
-                    longBreak++;
-                    currentBreak = true;
-                    Solution.paused = true;
-                    Solution.button.setText("Begin Break");
-                    Solution.testArea2.setText("3 Mins & 0 Seconds");
+                timeFinish= false;
+                --time;
+                if(currentBreak){
+                    Solution.testArea2.setText("BREAK " + Integer.toString(longBreak) +": "
+                            + Integer.toString( this.time/60) +
+                            " Mins & "  + Integer.toString(this.time%60) + " Seconds " );
                 }
-                // if the time is 0 and IS the 5th break while NOT being on break
-                else if(this.time == 0 && !currentBreak){
-                    longBreak =0;
+                else{
+                    Solution.testArea2.setText( Integer.toString( this.time/60)
+                            +  " Mins & "  + Integer.toString(this.time%60) + " Seconds "  );
+                }
+
+                //if time is 0
+                if(this.time == 0){
+                    timeFinish = true;
+                    //is NOT the 5th break and NOT already on break
+                    if(longBreak  != 4 && !currentBreak){
+                        //                    this.time = 180;
+                        this.time = 10;
+                        longBreak++;
+                        currentBreak = true;
+                        Solution.paused = true;
+                        Solution.button.setText("Begin Break");
+                        Solution.testArea2.setText("3 Mins & 0 Seconds");
+                    }
+                    //IS the 5th break while NOT being on break
+                    else if(!currentBreak){
+                        longBreak =0;
 //                    this.time = 900;
-                    this.time = 15;
-                    currentBreak = true;
-                    Solution.paused = true;
-                    Solution.button.setText("Begin Break");
-                    Solution.testArea2.setText("15 Mins & 0 Seconds");
-                }
-                //if the time is 0 and NOT on break
-                else if(this.time == 0){
-                    currentBreak = false;
-                    Solution.paused = true;
+                        this.time = 15;
+                        currentBreak = true;
+                        Solution.paused = true;
+                        Solution.button.setText("Begin Break");
+                        Solution.testArea2.setText("15 Mins & 0 Seconds");
+                    }
+                    //NOT on break
+                    else{
+                        currentBreak = false;
+                        Solution.paused = true;
 //                    this.time = 1500;
-                    this.time = 25;
-                    Solution.button.setText("Resume Work");
-                    Solution.testArea2.setText(Integer.toString( this.time/60)
-                            +  " Mins & "  + Integer.toString(this.time%60) + " Seconds ");
+                        this.time = 25;
+                        Solution.button.setText("Resume Work");
+                        Solution.testArea2.setText(Integer.toString( this.time/60)
+                                +  " Mins & "  + Integer.toString(this.time%60) + " Seconds ");
+                    }
                 }
 
                 try {
                     Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    System.out.println("GOT IN HERE");
-                    Solution.paused = true;
-                }
+                } catch (InterruptedException e) {}
+            }
+            if(timeFinish){
+                Solution.MGP.start(Solution.loop);
             }
 
             try {
--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -3,47 +3,79 @@
  * @since February 21, 1:52 PM
  */
 
+import sun.audio.AudioData;
+import sun.audio.AudioPlayer;
+import sun.audio.AudioStream;
+import sun.audio.ContinuousAudioDataStream;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.lang.Thread;
+import java.io.*;
+import sun.audio.*;
 public class Solution extends Thread {
 
-    static boolean running = false;
     static boolean paused = true;
     static boolean begin = false;
     static JTextArea testArea2 =new JTextArea("25 Mins & 00 Seconds");
     static JButton button = new JButton("Begin Timer!");
+    static AudioDataStream asd;
+    static AudioPlayer MGP = AudioPlayer.player;
+    static ContinuousAudioDataStream loop = null;
 
     public static void main(String[] args) {
-        running = true;
+
+        AudioStream BGM;
+        AudioData MD;
+
+
+        try {
+            BGM = new AudioStream(new FileInputStream("sound.wav"));
+            MD = BGM.getData();
+            loop = new ContinuousAudioDataStream(MD);
+            asd = new AudioDataStream(MD);
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         JFrame frame = new JFrame("Pomodoro Timer");
-
-
-
         JPanel panel = new JPanel();
         JTextArea testArea = new JTextArea("Pomodoro Timer");
 
         // Add button to JPanel
-        panel.add(testArea);
-        panel.add(testArea2);
-        panel.add(button);
+
+        panel.setLayout(null);
+
+        int xScreen = Toolkit.getDefaultToolkit().getScreenSize().width/4;
+        int yScreen = Toolkit.getDefaultToolkit().getScreenSize().height/4;
         // And JPanel needs to be added to the JFrame itself!
         frame.getContentPane().add(panel);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(Toolkit.getDefaultToolkit().getScreenSize().width/4,Toolkit.getDefaultToolkit().getScreenSize().height/2);
+        frame.setSize(xScreen, yScreen);
 
+
+        testArea.setBounds(xScreen/3,0,200,20);
+        testArea2.setBounds(xScreen/3,yScreen/8,200,20);
+        button.setBounds(xScreen/3,yScreen/4,200,30);
+        panel.add(testArea);
+        panel.add(testArea2);
+        panel.add(button);
         PomodoroThread p = new PomodoroThread();
         Thread t1= new Thread(p);
-
-
 
 
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+               MGP.stop(loop);
+
                 if(!begin){
                     begin = true;
                 }
